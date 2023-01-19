62d53b2aaa50f164eac8eed030557b5463c16d1a
AngeloCatarozzolo <angelocatarozzolo@gmail.com>
2023-01-19 10:23:02 +0100

    fix

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Train;
 use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
@@ -15,18 +16,29 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for($i = 0; $i <= 20; $i++) {
+        // ciclo per compilare i dati x50
+        for($i = 0; $i <= 50; $i++) {
+
         // istanziare un ogetto del modello
         $new_train = new Train();
+
+        // compilare i campi della tabella
         $new_train->company = $faker->company();
         $new_train->departure_station = $faker->city();
         $new_train->arrival_station = $faker->city();
-        $new_train->departure_time = $faker->time();
-        $new_train->arrival_time = $faker->time();
-        $new_train->code = randomFloat([100, 999]);
-        $new_train->wagon = $faker->numberBetween([5, 10]);
-        $new_train->is_in_time = rand(0,1);
-        $new_train->is_cancelled = rand(0,1);
+
+        //per non duplicare il dato tra le stazioni di partenza/arrivo
+        while($new_train->departure_station === $new_train->arrival_station){
+            $new_train->arrival_station = $faker->city();
+        }
+
+        $new_train->departure_time = $faker->dateTimeBetween('-1 day', '+2 days');
+        $new_train->arrival_time = $faker->dateTimeBetween('-1 day', '+2 days');
+        $new_train->code = $faker->bothify(['?????#####']);
+        $new_train->wagon = $faker->numberBetween([1, 10]);
+        $new_train->is_in_time = $faker->boolean(80);
+        $new_train->is_cancelled = $faker->boolean(10);
+        
         // salva il record nel DB
         $new_train->save();
         }
